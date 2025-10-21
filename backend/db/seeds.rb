5c8db758350e88e108c9ab208413e23a2134eaa3
Hassyunity <hassy.tsihoarana@gmail.com>
2025-10-21 18:05:41 +0300

    add seeds

--- backend/db/seeds.rb
+++ backend/db/seeds.rb
@@ -1,9 +1,123 @@
-# This file should ensure the existence of records required to run the application in every environment (production,
-# development, test). The code here should be idempotent so that it can be executed at any point in every environment.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Example:
-#
-#   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
-#     MovieGenre.find_or_create_by!(name: genre_name)
-#   end
+Article.create([
+  {
+    nom: "gourde coloré",
+    prix_initiale: 9916.0,
+    prix_vente: 25000.0,
+    quantite: 2,
+    status: "disponible",
+    remarque: nil,
+    reste: 2,
+    total_ventes: 0.0,
+    image: "gourde.jpg"
+  },
+  {
+    nom: "Black pink",
+    prix_initiale: 10050.0,
+    prix_vente: 42000.0,
+    quantite: 20,
+    status: "disponible",
+    remarque: nil,
+    reste: 6,
+    total_ventes: 588000.0,
+    image: "Black_pink.jpg"
+  },
+  {
+    nom: "Hoodie",
+    prix_initiale: 26130.0,
+    prix_vente: 65000.0,
+    quantite: 3,
+    status: "disponible",
+    remarque: nil,
+    reste: 3,
+    total_ventes: 0.0,
+    image: "Hoodie.jpg"
+  },
+  {
+    nom: "Guitare - M",
+    prix_initiale: 66330.0,
+    prix_vente: 175000.0,
+    quantite: 5,
+    status: "disponible",
+    remarque: nil,
+    reste: 5,
+    total_ventes: 0.0,
+    image: "GuitareM.jpg"
+  },
+  {
+    nom: "Guitare - H",
+    prix_initiale: 97820.0,
+    prix_vente: 250000.0,
+    quantite: 2,
+    status: "disponible",
+    remarque: nil,
+    reste: 2,
+    total_ventes: 0.0,
+    image: "GuitareH.jpg"
+  },
+  {
+    nom: "Casque",
+    prix_initiale: 32160.0,
+    prix_vente: 80000.0,
+    quantite: 10,
+    status: "disponible",
+    remarque: nil,
+    reste: 10,
+    total_ventes: 0.0,
+    image: "Casque.jpg"
+  },
+  {
+    nom: "Protection telephone",
+    prix_initiale: 5360.0,
+    prix_vente: 7000.0,
+    quantite: 16,
+    status: "disponible",
+    remarque: nil,
+    reste: 16,
+    total_ventes: 0.0,
+    image: "Protection_telephone.jpg"
+  },
+  {
+    nom: "Sac de sport",
+    prix_initiale: 25460.0,
+    prix_vente: 70000.0,
+    quantite: 5,
+    status: "disponible",
+    remarque: nil,
+    reste: 5,
+    total_ventes: 0.0,
+    image: "Sac_de_sport.jpg"
+  },
+  {
+    nom: "Pose telephone",
+    prix_initiale: 3886.0,
+    prix_vente: 12000.0,
+    quantite: 10,
+    status: "disponible",
+    remarque: nil,
+    reste: 10,
+    total_ventes: 0.0,
+    image: "Pose_telephone.jpg"
+  },
+  {
+    nom: "Support voyage",
+    prix_initiale: 6388.0,
+    prix_vente: 10000.0,
+    quantite: 10,
+    status: "disponible",
+    remarque: nil,
+    reste: 10,
+    total_ventes: 0.0,
+    image: "Support_voyage.jpg"
+  },
+  {
+    nom: "Patch fluo pour roue",
+    prix_initiale: 4683.0,
+    prix_vente: 10000.0,
+    quantite: 5,
+    status: "disponible",
+    remarque: nil,
+    reste: 5,
+    total_ventes: 0.0,
+    image: "Patch_fluo_pour_roue.jpg"
+  }
+])
