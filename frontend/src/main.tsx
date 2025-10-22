d8b8f26516346e9d3fa1303910fb619e426e4f1e
Hassyunity <hassy.tsihoarana@gmail.com>
2025-10-22 08:30:20 +0300

    add wake up backend for render free

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -3,9 +3,20 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App.tsx';
 
+const wakeUpBackend = async () => {
+  const BACKEND_URL = "https://babel-goods-api.onrender.com/";
+  try {
+    await fetch(`${BACKEND_URL}/healthcheck`, { cache: "no-cache" });
+    console.log("✅ Backend réveillé !");
+  } catch (err) {
+    console.warn("⚠️ Backend injoignable :", err);
+  }
+};
+
+wakeUpBackend();
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
   </StrictMode>
 );
-
